5cd5020d6288bb214e5ffbb96cf0de13ff3d91b7
ajps208 <ajithps208@gmail.com>
2026-01-20 22:33:52 +0530

    feat: add submission api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import recordRoutes from "./routes/record.routes.js";
 
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/records", recordRoutes);
 
 
 export default app;
