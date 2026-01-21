06c865f4a2eb306b9a762ce74abddde11c610953
ajps208 <ajithps208@gmail.com>
2026-01-21 13:56:08 +0530

    feat(backend): validate DOB format and use numeric nanoid

--- backend/src/routes/record.routes.js
+++ backend/src/routes/record.routes.js
@@ -1,24 +1,25 @@
 import express from "express";
 import Record from "../models/Record.js";
-// UUID for generating unique IDs
-import { v4 as uuid } from "uuid";
+import { generateNumericId } from "../helpers/helpers.js";
 
 const router = express.Router();
 
 // Regular expressions
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const phoneRegex = /^\+[1-9]\d{6,14}$/; 
+const phoneRegex = /^\+[1-9]\d{6,14}$/;
 const urlRegex = /^(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/;
+const dobRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 
 // Create record endpoint(POST http://localhost:5000/api/records)
 router.post("/", async (req, res) => {
   try {
-
     const { name, email, phoneNumber, link, dob } = req?.body;
 
     // Required fields
     if (!name || !email || !phoneNumber) {
-      return res.status(400).json({ message: "Name, Email and Phone are required" });
+      return res
+        .status(400)
+        .json({ message: "Name, Email and Phone are required" });
     }
 
     // Email
@@ -29,7 +30,7 @@ router.post("/", async (req, res) => {
     // Phone (supports multiple country codes)
     if (!phoneRegex.test(phoneNumber)) {
       return res.status(400).json({
-        message: "Invalid phone number. Use country code (e.g. +919876543210)"
+        message: "Invalid phone number. Use country code (e.g. +919876543210)",
       });
     }
 
@@ -39,22 +40,34 @@ router.post("/", async (req, res) => {
     }
 
     // Optional DOB
-    if (dob && isNaN(new Date(dob))) {
-      return res.status(400).json({ message: "Invalid DOB" });
+    let dobDate;
+    if (dob) {
+      if (!dobRegex.test(dob)) {
+        return res.status(400).json({ message: "DOB must be DD/MM/YYYY" });
+      }
+
+      const [day, month, year] = dob.split("/");
+      dobDate = new Date(Date.UTC(year, month - 1, day));
+
+      if (isNaN(dobDate)) {
+        return res.status(400).json({ message: "Invalid DOB date" });
+      }
     }
 
     const record = await Record.create({
-      uid: uuid(),
+      uid: generateNumericId(),
       name: name.trim(),
       email: email.toLowerCase(),
       phoneNumber,
-      link,
-      dob,
-      status: "PENDING"
+      ...(link && { link }),
+      ...(dobDate && { dob: dobDate }),
+      status: "PENDING",
     });
 
     res.status(201).json(record);
   } catch (err) {
+    console.log(err);
+    
     res.status(500).json({ message: "Server error" });
   }
 });
