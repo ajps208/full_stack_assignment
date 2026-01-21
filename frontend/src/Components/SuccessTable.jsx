e9169e07a3075f4bf2b458284a666b2b27942500
ajps208 <ajithps208@gmail.com>
2026-01-21 17:17:25 +0530

    feat(frontend): display successful records

--- frontend/src/Components/SuccessTable.jsx
+++ frontend/src/Components/SuccessTable.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export const SuccessTable = () => {
+
+export const  SuccessTable = ({ data }) => {
   return (
-    <div>SuccessTable</div>
-  )
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Name</th><th>Email</th><th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(d => (
+          <tr key={d._id}>
+            <td>{d.name}</td>
+            <td>{d.email}</td>
+            <td>{d.phoneNumber}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
