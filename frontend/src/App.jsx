d3f8f9a050b3d73c994a39bc4b9e7916d65a37b7
ajps208 <ajithps208@gmail.com>
2026-01-21 20:39:53 +0530

    style(frontend): apply global layout styling to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,15 @@ import { Loader } from "./Components/Loader";
 import { Header } from "./Components/Header";
 import { Form } from "./Components/Form";
 import { SuccessTable } from "./Components/SuccessTable";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-<ToastContainer autoClose={3000} />;
 
 export default function App() {
   const [view, setView] = useState("form");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // fetch success data
+  // fetch data
   useEffect(() => {
     if (view === "success") {
       setLoading(true);
@@ -27,16 +24,24 @@ export default function App() {
   }, [view]);
 
   return (
-    <>
-    {/* header */}
+    <div className="bg-[#f8fafc] text-slate-900 antialiased mih-h-screen">
+      {/* Header */}
       <Header view={view} setView={setView} />
-      {/* show form  and success table*/}
-      {view === "form" && <Form />}
-      {view === "success" &&
-        (loading ? <Loader /> : <SuccessTable data={data} />)}
 
-      {/* show toast */}
-      <ToastContainer position="top-right" autoClose={3000} />
-    </>
+      {/* Main */}
+      <main className="max-w-4xl mx-auto px-4 py-6">
+        <div className="bg-white rounded-2xl border border-slate-100 shadow-lg">
+          <div className="p-6">
+            {/*form or success table  */}
+            {view === "form" && <Form />}
+            {view === "success" &&
+              (loading ? <Loader /> : <SuccessTable data={data} />)}
+          </div>
+        </div>
+      </main>
+      
+      {/* Toast container to notify */}
+      <ToastContainer position="bottom-center" autoClose={3000} />
+    </div>
   );
 }
